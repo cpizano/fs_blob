4ec2b963ce15b4691d72a363b4f2075b9b63fe2a
Carlos Pizano <cpu@google.com>
2021-11-27 18:09:00 -0800

    first commit

---
+++ main.cc
@@ -0,0 +1,12 @@
+// cpu@ 2021 report-of-reports generator
+// see the README.txt file for more info.
+
+#include <string>
+
+
+int main() {
+  int index = 0;
+  printf("what %d \n", index);
+
+  return 0;
+}
