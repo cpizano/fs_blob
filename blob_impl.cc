ca77643c3f1e2a3d4b212bda7cc095298e1b05a2
Carlos Pizano <cpu@google.com>
2021-12-23 15:54:44 -0800

    add text logging to blobimpl

--- blob_impl.cc
+++ blob_impl.cc
@@ -1,16 +1,43 @@
 #include "blob.h"
 #include <unordered_map>
 
+#include <ctype.h>
+#include <stdio.h>
+
+namespace {
+
+void hexdump(const void *ptr, int buflen, int cols = 16) {
+  auto *buf = (const unsigned char*)ptr;
+  int i, j;
+  for (i = 0; i < buflen; i+= cols) {
+    printf("%06x: ", i);
+    for (j = 0; j < cols; j++) 
+      if (i + j < buflen)
+        printf("%02x ", buf[i + j]);
+      else
+        printf("   ");
+    printf(" ");
+    for (j = 0; j < cols; j++) 
+      if ( i + j < buflen)
+        printf("%c", isprint(buf[i+j]) ? buf[i+j] : '.');
+    printf("\n");
+  }
+}
+
+}
+
+
 class BlobStoreImpl;
 
 class BlobImpl : public Blob {
  public:
-  BlobImpl(BlobStoreImpl* bs) : bs_(bs) {}
+  BlobImpl(uint64_t id, BlobStoreImpl* bs) : id_(id), bs_(bs) {}
   const Data& Get() const override;
   int Put(const Data& data) override;
   int Release() override;
 
  private:
+  const uint64_t id_;
   Data data_;
   BlobStoreImpl* const bs_;
 };
@@ -24,7 +51,7 @@ class BlobStoreImpl : public BlobStore {
   Blob* GetBlob(uint64_t) override;
   uint64_t GetFreeSpace() override;
 
-  int Store(const Data&);
+  int Store(const Data&, uint64_t id);
 
  private:
   BlobMap bmap_;
@@ -47,7 +74,7 @@ Blob* BlobStoreImpl::GetBlob(uint64_t id) {
     return item->second;
   }
 
-  auto bs = new BlobImpl(this);
+  auto bs = new BlobImpl(id, this);
   bmap_[id] = bs;
   return bs;
 }
@@ -56,9 +83,12 @@ uint64_t BlobStoreImpl::GetFreeSpace() {
   return free_space_;
 }
 
-int BlobStoreImpl::Store(const Data& data) {
+int BlobStoreImpl::Store(const Data& data, uint64_t id) {
   free_space_-= data.size();
   // $fixme: actually store? or do it at Release() time.
+  printf(">> 0x%x  sz: %zu\n", id, data.size());
+  hexdump(&data[0], data.size());
+
   return 0;
 }
 
@@ -76,7 +106,7 @@ int BlobImpl::Put(const Data& data) {
   }
 
   data_ = data;
-  return bs_->Store(data_);
+  return bs_->Store(data_, id_);
 }
 
 int BlobImpl::Release() {
